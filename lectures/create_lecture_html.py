d790888039842c6b1c01aed1b41bcafd7b86f297
Adam Prugel-Bennett <apb@ecs.soton.ac.uk>
2026-03-23 17:30:31 +0000

    "update lectures"

--- lectures/create_lecture_html.py
+++ lectures/create_lecture_html.py
@@ -53,7 +53,7 @@ def getKeywords(file) :
 for lecture in lectures:
     tex = f"{lecture}.tex"
     lesson, keywords = getKeywords(tex)
-    html.write(f"<div id=\"{lesson}\">")
+    html.write(f"<div id=\"{lecture}\">")
     html.write(f"<li><b>{lesson}</b>: {keywords}\n")
     html.write(f"<ul> <li> <a href=\"lectures_pdf/{lecture}.pdf\">Lecture PDF</a>,\n")
     html.write(f"  <a href=\"lectures_pdf/{lecture}_prn.pdf\">Printable PDF</a>,\n")
